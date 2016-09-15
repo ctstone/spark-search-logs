f94d0b3968e4ba9b3524c165c5912dae7ea55373
Chris Stone <chstone@microsoft.com>
2016-09-15 21:39:24 +0000

    added time window stats

--- src/main/scala/SearchLogsApp.scala
+++ src/main/scala/SearchLogsApp.scala
@@ -1,12 +1,18 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.{ StructType, StructField, TimestampType, StringType, IntegerType }
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
 
 object MyApp extends App {
-  val spark = SparkSession.builder()
-    .master("spark://chstone2:7077")
-    .config("spark.jars", "/usr/hadoop/share/hadoop/tools/lib/hadoop-azure-2.7.3.jar,/usr/hadoop/share/hadoop/tools/lib/azure-storage-2.0.0.jar")
+  // TODO collect these from args
+  val input = "wasbs://nbcs@chstone.blob.core.windows.net/*.json.gz"
+  val output = "wasbs://nbcs-stats@chstone.blob.core.windows.net/stats.json"
+
+  val spark = SparkSession
+    .builder
     .appName("Search Logs")
     .getOrCreate()
+
+  import spark.sqlContext.implicits._
 
   val schema = new StructType()
     .add("time", TimestampType)
@@ -30,8 +36,18 @@ object MyApp extends App {
 
   val df = spark.read
     .schema(schema)
-    .json("wasbs://nbcs@chstone.blob.core.windows.net/2016-08-01-*.json.gz")
+    .json(input)
 
-  df.printSchema
+  val oneHour = window($"time", "1 hour")
+
+  df
+    .groupBy(oneHour, $"operationName")
+    .agg(
+      count("operationName").as("total"),
+      avg("durationMS").as("avgLatency"))
+    .orderBy("window")
+    .write
+    .json(output)
+
   spark.stop
 }
